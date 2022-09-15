d50cf1c70a69ec3ef7313efd4c26e607e1df6c91
luisgfmarques <luisgustavomarques@outlook.com>
2022-09-15 17:12:09 -0300

    include final files
    Co-authored-by: ArthurMeloG<arthurdemelo2001@gmail.com>

--- graph.js
+++ graph.js
@@ -1,11 +1,26 @@
 
-
 function calculate_distance(pos1, pos2) {
-    return Math.sqrt((pos1.longitude - pos2.longitude) ** 2 + (pos1.latitude - pos2.latitude) ** 2);
+    const lon = (parseInt(pos1.longitude) - parseInt(pos2.longitude)) * 60 + (pos1.longitude - parseInt(pos1.longitude)) - (pos2.longitude - parseInt(pos2.longitude));
+    const lat = (parseInt(pos1.latitude) - parseInt(pos2.latitude)) * 60 + (pos1.latitude - parseInt(pos1.latitude)) - (pos2.latitude - parseInt(pos2.latitude));
+    return (Math.sqrt((lon) ** 2 + (lat) ** 2));
 }
 
-function Gerar_grafo(cidades , range_cabos) {
-    const size = (cidades.length);
+
+// function calculate_distance(pos1, pos2) {
+//     var graus_radiano = function (deg) { deg * (Math.PI / 180); };
+//     var R = 6371; // Raio da Terra em km
+//     var dLat = graus_radiano(pos2.latitude - pos1.latitude);
+//     var dLon = graus_radiano(pos2.longitude - pos1.longitude);
+//     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//         Math.cos(graus_radiano(pos1.latitude)) * Math.cos(graus_radiano(pos2.latitude)) *
+//         Math.sin(dLon / 2) * Math.sin(dLon / 2);
+//     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//     var d = R * c;
+//     return d;
+// }
+
+function Gerar_grafo(cidades, range_cabos) {
+    const size = cidades.length;
     var graph = new Array(size);
     // Zerar matriz de adjacencia
     for (let i = 0; i < size; i++) {
@@ -18,7 +33,7 @@ function Gerar_grafo(cidades , range_cabos) {
         for (let k = 0; k < size; k++) {
             const distance = calculate_distance(cidades[i], cidades[k])
             if (distance <= range_cabos && i != k)
-                graph[i][k] = (distance).toFixed(4);
+                 graph[i][k] = parseFloat((distance).toFixed(2));
             else
                 graph[i][k] = Infinity;
         }
@@ -27,49 +42,50 @@ function Gerar_grafo(cidades , range_cabos) {
 }
 
 function Dijask(initial_index, final_index, graph) {
-    var priorityquery = [];
-    var visited = [];
-    const grafosize = graph.length;
-    var set = new Array(grafosize);
-    for (let i = 0; i < grafosize; i++) {
-        if (graph[initial_index][i] != Infinity)
-            priorityquery.push([graph[initial_index][i], i, 0]);
-        set[i] = [Infinity, null];
+    let size = graph.length;
+    var result = new Array(size);
+    var dist = new Array(size);
+    var prev = new Array(size);
+    var visited = new Array(size);
+    for (let i = 0; i < size; i++) {
+        dist[i] = Infinity;
+        prev[i] = null;
+        visited[i] = false;
     }
-    set[initial_index] = [0, initial_index];
-    visited.push(initial_index);
-    priorityquery.sort((a, b) => a[0] - b[0]);
-    while (priorityquery.length > 0) {
-        var current = priorityquery.shift();
-        if (current != undefined) {
-            if (current[0] != Infinity) {
-                if (set[current[1]][0] >= current[0]) {
-                    set[current[1]] = [current[0], current[2]];
-                }
-                if (current[1] == final_index) {
-                    return set;
-                }
-                for (let i = 0; i < grafosize; i++) {
-                    if ( graph[current[1]][i] != Infinity && !visited.includes(i) && set[i][0] >= graph[current[1]][i] ) {
-                        priorityquery.push([graph[current[1]][i] + set[current[1]][0],i, current[1], ]);
-                    }
-                }
-                for (let i = 0; i < priorityquery.length; i++) {
-                    if (priorityquery[i][0] >= set[priorityquery[i][1]][0]) {
-                        priorityquery.splice(i, 1);
-                    }
-                }
+    dist[initial_index] = 0;
+    for (let i = 0; i < size; i++) {
+        let min = Infinity;
+        let u = 0;
+        for (let k = 0; k < size; k++) {
+            if (visited[k] == false && dist[k] < min) {
+                min = dist[k];
+                u = k;
             }
-            visited.push(current[1]);
-            priorityquery.sort((a, b) => a[0] - b[0]);
+        }
+        visited[u] = true;
+        for (let k = 0; k < size; k++) {
+            if (visited[k] == false && graph[u][k] != Infinity && (dist[u] + graph[u][k]) < dist[k]) {
+                dist[k] = dist[u] + graph[u][k];
+                prev[k] = u;
+            }
         }
     }
-    if (set[final_index][0] == Infinity) {
-        return [];
+    let path = [];
+    let u = final_index;
+    while (prev[u] != null) {
+        path.push(u);
+        u = prev[u];
     }
-    return set;
+    path.push(u);
+    result = new Array(path.length);
+    for (let i = 0; i < path.length; i++) {
+        result[i] = new Array(2);
+        result[i][0] = path[i];
+        result[i][1] = dist[path[i]];
+    }
+    return result.reverse();
 }
 
+// console.log(Dijask(0, 20, Gerar_grafo(cidades, 350)));
+// console.table(Gerar_grafo(cidades, 350));
 export { Gerar_grafo, Dijask };
-// distancias em NM
-// cada 1NM equivale a 1852m, logo 10NM equivale a 18520m
--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 import { Gerar_grafo, Dijask } from './graph.js';
 import PromptSync from 'prompt-sync';
 import  cidades from './citys.js';
-import { resolve } from './knapsack.js';
 
 const input = PromptSync({ sigint: true });
+
+function changing(dijska, linhas_disponiveis) {
+    let change = [];
+    for (let i = 0; i < dijska.length; i++) {
+        change.push(cidades[dijska[i][0]]);
+    }
+    let data = change.sort((a, b) => b.quantidade - a.quantidade);
+    let result = [];
+    let sum = 0;
+    let i = 0;
+    for (i = 0; i < data.length; i++) {
+        if (sum + data[i].quantidade <= linhas_disponiveis) {
+            sum += data[i].quantidade;
+            result.push(data[i]);
+            linhas_disponiveis -= data[i].quantidade;
+        }
+        if (sum == linhas_disponiveis) break;
+    }
+    return result;
+}
+
 
 function printcities() {
     console.clear();
     console.log('Cidades:');
-    // for (let i = 0; i < cidades.length; i++) {
-    //     console.log(
-    //         i +
-    //             ' - ' +
-    //             cidades[i].estado +
-    //             ' - ' +
-    //             cidades[i].sigla +
-    //             ' -  DDD:' +
-    //             cidades[i].ddd +
-    //             ' -  Região:' +
-    //             cidades[i].regiao +
-    //             ' -  Quantidade de linhas:' +
-    //             cidades[i].quantidade,
-    //     );
-    // }
     console.table(cidades);
 
-    const response = input("Pressione para voltar para o menu");
+    input("Pressione para voltar para o menu");
     return 0;
 }
 
-function inputdata(dados) {
+function inputdata() {
     console.clear();
 
-    let start = input('Digite o numero da cidade de inicio: ');
-    let finish =   input('Digite o numero da cidade de destino: ');
-    let range =  input('Digite o alcance do cabo entre as cidades na unidade NM(nautic mile): ');
+    let start = input('Digite o DDD da cidade de inicio: ');
+    let finish =   input('Digite o DDD da cidade de destino: ');
+    let range =  input('Digite o alcance do cabo entre as cidades km: ');
     let quantidade = input('Digite a quantidade de linhas disponiveis: ');
     
     let data = {
@@ -44,63 +49,63 @@ function inputdata(dados) {
         quantidade: Number(quantidade),
     };
 
-    dados = {...data};
-    return dados;
+    return data;
 };
-
-function menorcaminho(dijask, start, finish) {
-    var path = [];
-    var current = finish;
-    while (current != start) {
-        path.push(current);
-        current = dijask[current][1];
-    }
-    path.push(start);
-    path.reverse();
-    return path;
-}
 
 function menu() {
     console.clear()
     let dados = {
-        start: 0,
-        finish: 0,
-        range: 0, 
+        start: 11,
+        finish: 21,
+        range: 350, 
         quantidade: 0,
     };
     let option = 0;
     while (option != 5) {
-        console.log("Digite o numero da opção desejada: \n 1 - Ver cidades  \n 2 - Inserir dados \n 3 - Gerar menor caminho \n 4 - Gerar menor caminho com knapsack \n 5 - Sair");
+        console.log("Digite o numero da opção desejada: \n 1 - Ver cidades  \n 2 - Inserir dados \n 3 - Gerar caminho de menor custo\n 4 - Escolher cidades para instalar antena \n 5 - Sair");
         option = Number(input(""));
         let graph = [];
         let dijask = [];
-    
+        let result = [];
         switch (option) {
             case 1:
                 printcities();
                 break;
             case 2:
-                dados = inputdata(dados);
+                dados = inputdata();
                 console.table(dados);
                 break;
             case 3:
                 graph = Gerar_grafo(cidades, dados.range);
-                console.log("Grafo gerado com sucesso!");
-                dijask = Dijask(dados.start, dados.finish, graph);
-                console.log("Menor caminho gerado com sucesso!");
-                console.log("Menor caminho: " + menorcaminho(dijask, dados.start, dados.finish));
-                console.log("O menor caminho é");
-                let string = ""
-                let menor = menorcaminho(dijask, dados.start, dados.finish);
-                for (let i = 0; i < menor.length; i++) {
-                    string =  string + 'Estado: ' + cidades[menor[i]].sigla + " DDD: " + cidades[menor[i]].ddd + " -> " ;
+                dijask = Dijask(cidades.indexOf(cidades.find(elemento => elemento.ddd == dados.start)), cidades.indexOf(cidades.find(elemento => elemento.ddd == dados.finish)), graph);
+                if (dijask.length > 1) {
+                    console.log("Menor caminho gerado com sucesso!");
+                    console.log("A quantidade de cabos necessarios é: " + parseFloat(dijask[(dijask.length - 1)][1]).toFixed(2) + " km");
+                    let string = ""
+                    for (let i = 0; i < dijask.length; i++) {
+                        string = string + 'Estado: ' + cidades[dijask[i][0]].sigla + " DDD: " + cidades[dijask[i][0]].ddd;
+                        result.push(cidades[dijask[i][0]]);
+                        if(i != dijask.length - 1){
+                            string += " -> ";
+                        }
+                    }
+                    console.log(string);
                 }
-                console.log(string);
+                else
+                    console.log("Não foi possivel encontrar um caminho com as restrições impostas!");
                 break;
             case 4:
                 graph = Gerar_grafo(cidades, dados.range);
-                dijask = Dijask(dados.start, dados.finish, graph);
-                let knapsack = resolve(dados.quantidade, dijask);
+                dijask = Dijask(cidades.indexOf(cidades.find(elemento => elemento.ddd == dados.start)), cidades.indexOf(cidades.find(elemento => elemento.ddd == dados.finish)), graph);
+                let change = changing(dijask, dados.quantidade);
+                if (change.length > 0) {
+                    console.log("Cidades que devem ser cobertas:");
+                    for (let i = 0; i < change.length; i++) {
+                        console.log('Estado: ' + change[i].sigla + " DDD: " + change[i].ddd + " -> " + "Quantidade de linhas: " + change[i].quantidade + " ");
+                    }
+                }
+                else 
+                    console.log("Nenhuma cidade deve ser coberta!");
                 break;
             case 5:
                 console.log("Saindo...");
